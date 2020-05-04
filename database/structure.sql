d0476ba7cc439318926273b84dc0c5f98500384c
Mauricio Lima <github@mauriciolima.eti.br>
2020-05-03 23:42:45 -0300

    TB_EQUIPE - Team table definition

--- database/structure.sql
+++ database/structure.sql
@@ -11,6 +11,19 @@ MySQL - 10.1.33-MariaDB : Database - unicsul_f1
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `tb_equipe` */
+
+DROP TABLE IF EXISTS `tb_equipe`;
+
+CREATE TABLE `tb_equipe` (
+  `ID_EQUIPE` bigint(20) NOT NULL COMMENT 'Equipe - Chave primária',
+  `NM_EQUIPE` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Nome da equipe',
+  `ID_PAIS` bigint(20) DEFAULT NULL COMMENT 'País - chave estrangeira',
+  PRIMARY KEY (`ID_EQUIPE`),
+  KEY `FK_PAIS` (`ID_PAIS`),
+  CONSTRAINT `FK_PAIS` FOREIGN KEY (`ID_PAIS`) REFERENCES `tb_pais` (`ID_PAIS`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 /*Table structure for table `tb_pais` */
 
 DROP TABLE IF EXISTS `tb_pais`;
