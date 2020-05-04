7475a9fb1426ee3327f8a6cd0689eb239929b0d1
Mauricio Lima <github@mauriciolima.eti.br>
2020-05-04 00:27:46 -0300

    TB_PROVA - Round table definition

--- database/structure.sql
+++ database/structure.sql
@@ -57,13 +57,27 @@ CREATE TABLE `tb_piloto` (
   `ID_PILOTO` bigint(20) NOT NULL COMMENT 'Piloto - Chave primária',
   `NM_PILOTO` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Nome do piloto',
   `DT_NASCIMENTO` date DEFAULT NULL COMMENT 'Data de nascimento do piloto',
-  `ID_PAIS` bigint(20) DEFAULT NULL,
-  `ID_EQUIPE` bigint(20) DEFAULT NULL,
+  `ID_PAIS` bigint(20) DEFAULT NULL COMMENT 'País - chave estrangeira',
+  `ID_EQUIPE` bigint(20) DEFAULT NULL COMMENT 'Equipe - chave estrangeira',
   PRIMARY KEY (`ID_PILOTO`),
   KEY `FK_PILOTO_PAIS` (`ID_PAIS`),
   KEY `FK_PILOTO_EQUIPE` (`ID_EQUIPE`),
   CONSTRAINT `FK_PILOTO_EQUIPE` FOREIGN KEY (`ID_EQUIPE`) REFERENCES `tb_equipe` (`ID_EQUIPE`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `FK_PILOTO_PAIS` FOREIGN KEY (`ID_PAIS`) REFERENCES `tb_pais` (`ID_PAIS`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `tb_prova` */
+
+DROP TABLE IF EXISTS `tb_prova`;
+
+CREATE TABLE `tb_prova` (
+  `ID_PROVA` bigint(20) NOT NULL COMMENT 'Prova - Chave primária',
+  `DT_PROVA` date DEFAULT NULL COMMENT 'Data de realização da prova',
+  `NM_SITUACAO` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ID_CIRCUITO` bigint(20) DEFAULT NULL COMMENT 'Circuito da prova - chave estrangeira',
+  PRIMARY KEY (`ID_PROVA`),
+  KEY `FK_PROVA_CIRCUITO` (`ID_CIRCUITO`),
+  CONSTRAINT `FK_PROVA_CIRCUITO` FOREIGN KEY (`ID_CIRCUITO`) REFERENCES `tb_circuito` (`ID_CIRCUITO`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
